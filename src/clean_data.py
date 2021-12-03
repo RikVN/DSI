033293575d2a4af5e96348ede7809298a2c9c573
RikVN <p266548@let10.housing.rug.nl>
2021-12-03 16:36:37 +0100

    Updates for cleaning and splitting data

--- src/clean_data.py
+++ src/clean_data.py
@@ -10,97 +10,103 @@ from utils import write_to_file, setup_spacy_tokenizer
 
 
 def create_arg_parser():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-i", "--input_file", required=True, type=str,
-						help="Input file with crawled text")
-	parser.add_argument("-ml", "--min_length", default=5, type=int,
-						help="Minimum sentence length to be included")
-	parser.add_argument("-o", "--output_file", default='', type=str,
-						help="Output file with cleaned data. Default: add .clean to -i")
-	parser.add_argument("-l", "--lang", default='en', type=str,
-						help="Iso code of language we are cleaning")
-	args = parser.parse_args()
-	return args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input_file", required=True, type=str,
+                        help="Input file with crawled text")
+    parser.add_argument("-minl", "--min_length", default=7, type=int,
+                        help="Minimum sentence length to be included")
+    parser.add_argument("-maxl", "--max_length", default=50, type=int,
+                        help="Minimum sentence length to be included")
+    parser.add_argument("-o", "--output_file", default='', type=str,
+                        help="Output file with cleaned data. Default: add .clean to -i")
+    parser.add_argument("-l", "--lang", default='en', type=str,
+                        help="Iso code of language we are cleaning")
+    args = parser.parse_args()
+    return args
 
 
-def length_check(line, tokenizer, min_length):
-	'''Check if the sentence passed the length check'''
-	return len(tokenizer(line)) >= min_length
+def length_check(line, tokenizer, min_length, max_length):
+    '''Check if the sentence passed the length check'''
+    tok_len = len(tokenizer(line))
+    return min_length <= tok_len <= max_length
 
 
 def ends_with_punctuation(line):
-	'''Check string ends with something we consider punctuation, see config.py'''
-	return line[-1] in punctuation
+    '''Check string ends with something we consider punctuation, see config.py'''
+    return line[-1] in punctuation
 
 
 def filter_doubles_from_list(items):
-	'''Filter double items in a list but keep order'''
-	return list(dict.fromkeys(items))
+    '''Filter double items in a list but keep order'''
+    return list(dict.fromkeys(items))
 
 
 def normalize(line):
-	'''Normalize a string'''
-	# Resolve multiple spaces
-	line = " ".join(line.split())
-	# Pairs of replacements here, for quotes/dashes
-	# See config.py for pairs of replacements
-	for char, replace_with in replacements:
-		line = line.replace(char, replace_with)
-	return line
+    '''Normalize a string'''
+    # Resolve multiple spaces
+    line = " ".join(line.split())
+    # Pairs of replacements here, for quotes/dashes
+    # See config.py for pairs of replacements
+    for char, replace_with in replacements:
+        line = line.replace(char, replace_with)
+    return line
 
 
 def correct_language(line, lang):
-	'''Check if a certain string belongs to the language we specified
-	   Use pycld3: https://github.com/bsolomon1124/pycld3'''
-	res = cld3.get_language(line)
-	return res.language == lang
+    '''Check if a certain string belongs to the language we specified
+       Use pycld3: https://github.com/bsolomon1124/pycld3'''
+    res = cld3.get_language(line)
+    return res.language == lang
 
 
 def specific_filter(line):
-	'''Data-specific things we found that we want to filter as well, not based on general principles'''
-	# The data of cybersecurity contains hundreds of these:
-	# No matching events listed under Workshops scheduled for DATE
-	# Just filter them
-	if line.startswith("No matching events listed under Workshops scheduled for"):
-		return True
-	return False
+    '''Data-specific things we found that we want to filter as well, not based on general principles'''
+    # The data of cybersecurity contains hundreds of these:
+    # No matching events listed under Workshops scheduled for DATE
+    # Just filter them
+    if line.startswith("No matching events listed under"):
+        return True
+    return False
 
 
-def clean_data(input_file, min_length, lang):
-	'''Clean the DSI data to only contain sentences/texts we want to keep'''
-	# Setup spacy tokenization for later checks
-	tokenizer = setup_spacy_tokenizer(lang)
+def clean_data(input_file, min_length, max_length, lang):
+    '''Clean the DSI data to only contain sentences/texts we want to keep'''
+    # Setup spacy tokenization for later checks
+    tokenizer = setup_spacy_tokenizer(lang)
 
-	# Now loop over the input sentences/texts and check if we keep them
-	keep_texts = []
-	for idx, line in enumerate(open(input_file, 'r')):
-		line = line.strip()
-		# First normalize the input: quotes, dashes, etc
-		line = normalize(line)
-		# Do a simple length check, which requires tokenization
-		if not length_check(line, tokenizer, min_length):
-			continue
-		# Sentences should end with punctuation of some sort
-		if not ends_with_punctuation(line):
-			continue
-		# The sentence should also be in the correct language (e.g. noticed some Russian for English)
-		if not correct_language(line, lang):
-			continue
-		# Data-specific things we found that we want to filter as well
-		if specific_filter(line):
-			continue
-		# Keep the final line
-		keep_texts.append(line)
+    # Now loop over the input sentences/texts and check if we keep them
+    keep_texts = []
+    for idx, line in enumerate(open(input_file, 'r')):
+        #if idx % 100000 == 0:
+        #    print (idx)
+        line = line.strip()
+        # First normalize the input: quotes, dashes, etc
+        line = normalize(line)
+        # Do a simple length check, which requires tokenization
+        if not length_check(line, tokenizer, min_length, max_length):
+            continue
+        # Sentences should end with punctuation of some sort
+        if not ends_with_punctuation(line):
+            continue
+        # The sentence should also be in the correct language (e.g. noticed some Russian for English)
+        if not correct_language(line, lang):
+            continue
+        # Data-specific things we found that we want to filter as well
+        if specific_filter(line):
+            continue
+        # Keep the final line
+        keep_texts.append(line)
 
-	# Filter double lines as well
-	keep_texts = filter_doubles_from_list(keep_texts)
-	print ("{0} of {1} lines remain for {2}".format(len(keep_texts), idx+1, input_file))
-	return keep_texts
+    # Filter double lines as well
+    keep_texts = filter_doubles_from_list(keep_texts)
+    print ("{0} of {1} lines remain for {2}".format(len(keep_texts), idx+1, input_file))
+    return keep_texts
+
 
 if __name__ == '__main__':
-	args = create_arg_parser()
-	# Clean the data here
-	clean_lines = clean_data(args.input_file, args.min_length, args.lang)
-	# Write them to output file
-	out_file = args.output_file if args.output_file else args.input_file + '.clean'
-	write_to_file(clean_lines, out_file)
+    args = create_arg_parser()
+    # Clean the data here
+    clean_lines = clean_data(args.input_file, args.min_length, args.max_length, args.lang)
+    # Write them to output file, add .clean to input if not specified
+    out_file = args.output_file if args.output_file else args.input_file + '.clean'
+    write_to_file(clean_lines, out_file)
--- src/split_data.py
+++ src/split_data.py
@@ -11,42 +11,70 @@ from utils import write_to_file
 
 
 def create_arg_parser():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-i", "--input_folder", required=True, type=str,
-						help="Main input folder")
-	parser.add_argument("-l", "--lang", default='en', type=str,
-						help="Iso code of language we are dealing with (default en)")
-	parser.add_argument("-t", "--test_size", default=5000, type=int,
-						help="Size of the test set, if total data > 2.5 * test size, else half")
-	args = parser.parse_args()
-	return args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input_folder", required=True, type=str,
+                        help="Main input folder")
+    parser.add_argument("-l", "--lang", default='en', type=str,
+                        help="Iso code of language we are dealing with (default en)")
+    parser.add_argument("-e", "--extension", default='.clean.tok.lower.dedup', type=str,
+                        help="Extension of the files we are splitting")
+    parser.add_argument("-fa", "--file_add", default='', type=str,
+                        help="What we add to the file names of train/dev/test, e.g. _clf for train_clf.ext")
+    parser.add_argument("-s", "--shuffle", action="store_true",
+                        help="Whether we shuffle the data before splitting")
+    parser.add_argument("-d", "--dev_size", default=1000, type=int,
+                        help="Size of the dev set")
+    parser.add_argument("-t", "--test_size", default=1000, type=int,
+                        help="Size of the test set")
+    parser.add_argument("-al", "--add_label", action="store_true",
+                        help="Whether we add the label as second item after the sentence (with tab).")
+    parser.add_argument("-mo", "--more_other", action="store_true",
+                        help="Add more 'other' instances, by doing: dev/testsize * num_dsis")
+    args = parser.parse_args()
+    return args
 
 
-def split_data(input_folder, test_size, lang):
-	'''Given an input folder, find the training files and split it'''
-	train_name = "train"
-	test_name = "test"
-	file_name = 'sentences'
-	file_ext = '.clean.tok.lower'
-	for idx, dsi in enumerate(dsis):
-		# Read in original file
-		cur_fol = input_folder + dsi + '/' + lang + '/'
-		orig = cur_fol + file_name + file_ext
-		lines = [x.strip() for x in open(orig, 'r')]
-		# Shuffle original file
-		shuffle(lines)
-		# Check if we change the test size
-		cur_test = test_size if not (len(lines) < (float(test_size) * 2)) else int(test_size / 2)
-		# Create splits
-		train = lines[cur_test:]
-		test = lines[:cur_test]
-		# Write to file
-		write_to_file(train, cur_fol + train_name + file_ext)
-		write_to_file(test, cur_fol + test_name + file_ext)
+def split_data(input_folder, d_size, t_size, lang, file_ext, do_shuffle, add_label, file_add, more_other):
+    '''Given an input folder, find the training files and split it'''
+    train_name = "train" + file_add
+    dev_name = "dev" + file_add
+    test_name = "test" + file_add
+    file_name = 'sentences'
+    for dsi in dsis:
+        # Read in original file
+        cur_fol = input_folder + dsi + '/' + lang + '/'
+        orig = cur_fol + file_name + file_ext
+        if os.path.isfile(orig):
+            lines = [x.strip() for x in open(orig, 'r')]
+            # Check if we found enough line so that training set is at least 3x dev + test
+            if len(lines) > 3 * (d_size + t_size):
+                # Shuffle original file if we want
+                if do_shuffle:
+                    shuffle(lines)
+                # Check if we want to add the label to the sentence
+                # This is easier for the classification task, where we need to keep this info
+                if add_label:
+                    lines = [x + '\t' + dsi for x in lines]
+
+                # Dev/test sizes are different for category "other" (maybe)
+                dev_size = (len(dsis) - 1) * d_size if more_other and dsi == "other" else d_size
+                test_size = (len(dsis) - 1) * t_size if more_other and dsi == "other" else t_size
+
+                # Create splits
+                train = lines[:len(lines) - dev_size - test_size]
+                dev = lines[len(train): len(train) + dev_size]
+                test = lines[len(train) + dev_size:]
+                # Write to file
+                write_to_file(train, cur_fol + train_name + file_ext)
+                write_to_file(dev, cur_fol + dev_name + file_ext)
+                write_to_file(test, cur_fol + test_name + file_ext)
+            else:
+                print ("Skipping {0}, length of {1} too small".format(orig, len(lines)))
+        else:
+            print ("Skipping {0}, file does not exist".format(orig))
 
 
 if __name__ == '__main__':
-	args = create_arg_parser()
-	# Split data sets here
-	split_data(args.input_folder, args.test_size, args.lang)
-
+    args = create_arg_parser()
+    # Split data sets here
+    split_data(args.input_folder, args.dev_size, args.test_size, args.lang, args.extension, args.shuffle, args.add_label, args.file_add, args.more_other)
